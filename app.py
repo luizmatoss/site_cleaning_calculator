b741731d3d29ff43ac265cc437bfd68ca1c9b409
luizmatoss <65014388+luizmatoss@users.noreply.github.com>
2024-10-03 19:59:51 +0100

    Update app.py
    CORS UPDATE

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Default Rates
 BASE_RATE = 0.15
